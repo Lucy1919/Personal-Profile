4b1a5b6d37f499f0309a40e707f3ef5843415b4c
Lucy1919 <lucynvani@gmail.com>
2025-09-03 19:47:52 -0700

    media complete

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } 
+  else {
+    document.body.classList.remove("dark-theme"); 
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -20,10 +45,9 @@ function contact(event) {
       alert(
         "The email service is temporarily unavailable. Please contact me directly at lucynvani@gmail.com"
       );
-    })
+    });
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
